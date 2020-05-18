cf9d84a977087465410588d859052a690185bd70
Masoud <gharagozlu.masoud@gmail.com>
2020-05-19 02:19:35 +0430

    Add words and fix bugs

--- private_html/messages/fa/words.php
+++ private_html/messages/fa/words.php
@@ -364,4 +364,7 @@ return [
     'En Material Required Amount'=> 'مقدار موردنیاز انگلیسی',
 
     'Section show page'=> 'نمایش بخش های پروژه',
+    'Acceleration and International Accounts Network'=> 'شبکه شتاب و حساب های بین المللی',
+    'Bank account number'=> 'شماره حساب بانکی',
+    'Approved phone number'=> 'شماره تماس تایید شده',
 ];

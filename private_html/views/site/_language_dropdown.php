8fdad71a51ba4e848a80b1bf35782c4efa6e34ce
Masoud <gharagozlu.masoud@gmail.com>
2020-06-01 21:53:50 +0430

    Fix bugs

--- private_html/views/site/_language_dropdown.php
+++ private_html/views/site/_language_dropdown.php
@@ -6,7 +6,7 @@ use yii\helpers\Url;
 ?>
 
 <div class="dropdown language-dropdown hidden-xs">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span>Language</span><i></i><?= trans('words', 'fa')?></a>
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span>Language</span><i></i><?= trans('words', app()->language)?></a>
     <ul class="dropdown-menu dropdown-menu-<?= app()->language == 'en' ? 'right' : 'left'?>">
         <li><a href="<?= Url::to(['/fa'])?>"><?= trans('words', 'fa')?></a></li>
         <li><a href="<?= Url::to(['/ar'])?>"><?= trans('words', 'ar')?></a></li>
